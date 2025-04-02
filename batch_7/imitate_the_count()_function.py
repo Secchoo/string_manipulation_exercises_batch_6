10c0e10d9c5a8fdf9b8ccfd656967cc0bba1d2fc
Justin cholo Adlawan <justincholo1234@gmail.com>
2025-04-02 17:46:14 +0800

    first draft for count program

---
+++ batch_7/imitate_the_count()_function.py
@@ -0,0 +1,18 @@
+#imitate the count() function without using count()
+
+"""
+This function takes a list and an element as input and 
+returns the number of occurrences of the element in the list.
+It uses a for loop to iterate through the list and a counter 
+variable to keep track of the number of occurrences.
+"""
+
+def imitate_count(string, sub):
+    count = 0
+    sub_len = len(sub)
+    if sub_len == 0:
+        return len(string) + 1
+    for i in range(len(string) - sub_len + 1):
+        if string[i:i + sub_len] == sub:
+            count += 1
+    return count
