8404cf0df9f3ebed309d89c57cd7f72da263d83a
Justin cholo Adlawan <justincholo1234@gmail.com>
2025-04-02 17:54:39 +0800

    code written will now test if it works

---
+++ batch_7/imitate_the_index()_function.py
@@ -0,0 +1,13 @@
+#imitate_the index() function without using index()
+
+"""
+This function takes a list and an element as input and
+returns the index of the first occurrence of the element in the list.
+"""
+
+def imitate_index(string, sub):
+    sub_len = len(sub)
+    for i in range(len(string) - sub_len + 1):
+        if string[i:i + sub_len] == sub:
+            return i
+    raise ValueError("substring not found")
