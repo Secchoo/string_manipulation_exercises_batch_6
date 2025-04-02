4896beb6a39ff3c51b1bbe22d39ff2970252f127
Justin cholo Adlawan <justincholo1234@gmail.com>
2025-04-02 22:02:21 +0800

    rstrip() program first error will try to fix

---
+++ batch_7/imitate_the_rstrip()_function.py
@@ -0,0 +1,14 @@
+#imitate rstrip() function without using rstrip()
+
+""""
+This function takes a string as input and returns the string 
+with trailing whitespace removed.
+"""
+
+def imitate_rstrip(string):
+    end = len(string)
+    while end > 0 and s[end - 1] == ' ':
+        end -= 1
+    return string[:end]
+
+print(imitate_rstrip("Hello World    "))
