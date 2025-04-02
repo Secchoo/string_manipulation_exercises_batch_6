c519e64b379fd76f5c6582f7cc398f5fa3439286
Justin cholo Adlawan <justincholo1234@gmail.com>
2025-04-02 22:31:55 +0800

    code written for zfill() program, code is now working properly

--- batch_7/imitate_the_zfill()_function.py
+++ batch_7/imitate_the_zfill()_function.py
@@ -1,2 +1,17 @@
+#imitate zfill() function without using zfill()
 
+"""
+This function pads the string on the left with zeros to fill a width.
+"""
+
+def imitate_zfill(string, width):
+    if len(string) >= width:
+        return string
+    sign = ''
+    if string.startswith(('+', '-')):
+        sign, string = string[0], string[1:]
+    return sign + '0' * (width - len(string) - len(sign)) + string
+
+
+print(imitate_zfill("42", 5))
 
