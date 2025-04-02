f946b23de2a7edebddcc72b6df6665e00f7f593a
Justin cholo Adlawan <justincholo1234@gmail.com>
2025-04-02 20:33:34 +0800

    started with the code of islower() program

---
+++ batch_7/imitate_the_islower()_function.py
@@ -0,0 +1,8 @@
+#imitate islower() function without using islower()
+
+"""
+This function takes a string as input and returns True 
+if all the characters in the string are lowercase letters, 
+and False otherwise.
+"""
+
