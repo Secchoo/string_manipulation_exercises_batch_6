5d918187d39482fd704c94c5d84f61577d3b43b0
Justin cholo Adlawan <justincholo1234@gmail.com>
2025-04-02 21:47:52 +0800

    code written for islower() program

--- batch_7/imitate_the_islower()_function.py
+++ batch_7/imitate_the_islower()_function.py
@@ -5,4 +5,13 @@ This function takes a string as input and returns True
 if all the characters in the string are lowercase letters, 
 and False otherwise.
 """
+def custom_islower(s):
+    has_cased = False
+    for c in s:
+        if c.isalpha():
+            has_cased = True
+            if not ('a' <= c <= 'z'):
+                return False
+    return has_cased
 
+print(custom_islower("hello"))  
